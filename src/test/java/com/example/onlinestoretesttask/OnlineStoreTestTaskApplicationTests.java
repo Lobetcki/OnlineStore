811b6adb4f2afef269248a89ff3a74d7d0a359f0
Lobetcki <Lobetcki@gmail.com>
2023-04-04 13:33:00 +0300

    Start OnlineStoreTestTask 04042023

---
+++ src/test/java/com/example/onlinestoretesttask/OnlineStoreTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.onlinestoretesttask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineStoreTestTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
